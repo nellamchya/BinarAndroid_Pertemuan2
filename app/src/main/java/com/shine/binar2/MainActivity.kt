8f9e0e7c1e965568b8b43524e9f6ab6cc15a9917
nellamchya <88300976+nellamchya@users.noreply.github.com>
2023-09-01 16:15:33 +0700

    Initials project Hello word

--- app/src/main/java/com/shine/binar2/MainActivity.kt
+++ app/src/main/java/com/shine/binar2/MainActivity.kt
@@ -1,11 +1,40 @@
-package com.shine.binar2
+package com.catnip.binar2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.catnip.binarch2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // private var tvHelloWorld : TextView? = null
+
+    //1. Declare binding and initiate as lazy
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //2. change layout name into binding root
+        setContentView(binding.root)
+        setTextHello()
+    }
+
+    private fun setTextHello() {
+        // Old way
+
+        //tvHelloWorld = findViewById(R.id.tv_hello_world)
+        //tvHelloWorld?.text = "text text text" // Setter
+        //Toast.makeText(this, tvHelloWorld?.text, Toast.LENGTH_SHORT).show() //getter
+
+        //View binding
+        binding.tvHelloWorld.text = "text text text"
+
+        //context in android
+        // Application context -> whole app context
+        // Activity context -> Di activity mana
     }
 }
